8390bd4a99878b9ca1ca82f879760357f3a90e5f
Chester <chester.h4@gmail.com>
2020-10-29 17:43:06 -0700

    basic 'command' recognized - TODO: refactor into a class

--- voice.py
+++ voice.py
@@ -1,40 +1,31 @@
 #!/usr/bin/env python3
 
-# NOTE: this example requires PyAudio because it uses the Microphone class
-
-import time
-
 import speech_recognition as sr
 
-
-# this is called from the background thread
-def callback(recognizer, audio):
-    # received audio data, now we'll recognize it using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
+# obtain audio from the microphone
 r = sr.Recognizer()
-m = sr.Microphone()
-with m as source:
-    r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
+with sr.Microphone() as source:
+    print("Say something!")
 
-# start listening in the background (note that we don't have to do this inside a `with` statement)
-stop_listening = r.listen_in_background(m, callback)
-# `stop_listening` is now a function that, when called, stops background listening
+    while True:
+        audio = 0
+        try:
+            audio = r.listen(source,1.5)
+        except sr.WaitTimeoutError:
+            print("No new words")
 
-# do some unrelated computations for 5 seconds
-for _ in range(50): time.sleep(0.1)  # we're still listening even though the main thread is doing other things
+        # recognize speech using Google Speech Recognition
+        output_string = ""
+        try:
+            output_string =  r.recognize_google(audio)
+            print("Google Speech Recognition thinks you said " + output_string)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        except:
+            print("no audio")
 
-# calling this function requests that the background listener stop listening
-stop_listening(wait_for_stop=False)
 
-# do some more unrelated things
-while True: time.sleep(0.1)  # we're not listening anymore, even though the background thread might still be running for a second or two while cleaning up and stopping
+        if (output_string=="hello"):
+            print("CALLBACK WE STARTED A POWERUP") #this will need to be fine tuned later
