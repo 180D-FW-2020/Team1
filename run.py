c4353bee5d7c09ac22e3ee5fbb758fd4880564a6
Shalin Shah <34790694+shahshalin@users.noreply.github.com>
2020-10-30 00:01:08 -0700

    Skeleton (#1)
    * major skeleton code updates and added TODOs/NOTEs for every function

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
+# file to run the program
 from PoseEstimationImg import *
 import cv2
 import time 
 import numpy as np
+cap = cv2.VideoCapture(0)
 
 pose = PoseEstimation()
-
-pose.testScoring()
+pose.startUI()
