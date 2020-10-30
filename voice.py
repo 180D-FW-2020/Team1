de3f2f9814b8a249c45845f3fb7ac787d934ef08
Chester <chester.h4@gmail.com>
2020-10-30 15:41:46 -0700

    Basic speech recognition class built (with an example

--- voice.py
+++ voice.py
@@ -1,27 +1,59 @@
-#!/usr/bin/env python3
-
 #requires pyAudio and Speech Recognition conda venv.
 
 import speech_recognition as sr
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
+#Class to recognize commands. Listen/stop are used to control it being active.
+#dispatch should be called in any free time the program has (where actual listening is performed)
+class commandRecognizer:
+    def __init__(self, command_dictionary):
+        self.command_dictionary = command_dictionary
+        self.active = False
 
-    while True:
-        audio = r.listen(source)
+    def listen(self):
+        self.active = True
+    
+    def stop(self):
+        self.active = False
 
-        # recognize speech using Google Speech Recognition
-        output_string = ""
-        try:
-            output_string =  r.recognize_google(audio)
-            print("Google Speech Recognition thinks you said " + output_string)
+    def dispatch(self):
+        if self.active:
+            # obtain audio from the microphone
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                print("Say something!")
 
-            keyword = "activate"
-            if (keyword.lower() in output_string.lower()):
-                print("CALLBACK WE STARTED A POWERUP") #this will need to be fine tuned later
+                audio = r.listen(source)
+                # recognize speech using Google Speech Recognition
+                output_string = ""
+                try:
+                    output_string =  r.recognize_google(audio)
+                    print("Google Speech Recognition thinks you said " + output_string)
 
-            
-        except Exception as e:
-            print("please try again")
+                    #check if the keyword is present in any of the speech
+                    for key in self.command_dictionary:
+                        if (key.lower() in output_string.lower()):
+                            print("CALLBACK WE STARTED A POWERUP") 
+                            self.command_dictionary[key]()
+
+                except Exception as e:
+                    print("please try again")
+
+
+#example usecase
+
+#example callbacks for commands/pause/any functionality we need
+def ex1():
+    print("first one called")
+def ex2():
+    print("second one called")
+
+#Example keywords being linked to the callbacks
+keywords_example = {
+    "activate" : ex1,
+    "help" : ex2
+}
+
+c = commandRecognizer(keywords_example)
+c.listen()
+while True:
+    c.dispatch()
