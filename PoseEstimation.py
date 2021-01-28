f881d9dbe2c880fa22870ed48901d7df0fc0d6ec
wsakutori <wsakutori@gmail.com>
2021-01-28 11:33:46 -1000

    Grayscale to BGR

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -172,12 +172,11 @@ class PoseEstimation():
             partB = pair[1]
 
             if points[partA] and points[partB]:
-                cv2.line(img, points[partA], points[partB], 255, thickness=80, lineType=cv2.FILLED)
+                cv2.line(img, points[partA], points[partB], (255, 255, 255), thickness=80, lineType=cv2.FILLED)
         
         if show:
             cv2.imshow('Output-Contour', img) 
             cv2.waitKey(0) 
-        img = np.float32(img)
         cv2.imwrite(output, img)
         
         return img, points
