18eaa3fcc3c3ebe58aa265618b4b2f298ec427c1
erykaxye <eryka1999@gmail.com>
2021-02-26 13:15:11 -0800

    updated setup script with correct versions

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 #list name of packages
 echo "installing base packages"
-conda create -y -n hitw python=3.6 numpy pandas joblib smbus2 paho-mqtt scikit-learn opencv=3.3.1 speechrecognition boto3 paramiko -c conda-forge
-conda activate hitw 
+conda create -y -n hitw python=3.8 numpy pandas joblib smbus2 paho-mqtt scikit-learn opencv=4.0.1 speechrecognition boto3 paramiko -c conda-forge
+# conda activate hitw 
 echo "finished" 
 
 # from distutils.core import setup, Extension 
